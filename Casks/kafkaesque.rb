519a7adbd4850c17e3a156961e896ad7260cebb6
Patrick Schuh <46817726+patschuh@users.noreply.github.com>
2023-10-04 07:52:53 +0200

    homebrew-esque 2.7.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Casks/kafkaesque.rb
+++ Casks/kafkaesque.rb
@@ -1,6 +1,6 @@
 cask "kafkaesque" do
-  version "2.6.0"
-  sha256 "59f4f3393b8cd703c454d08139e50bbd8eab431000884764be36ada4eadc3957"
+  version "2.7.0"
+  sha256 "d6950d7e7b1db89ae59175b5d9debbcf67d33b7d47ca8fda664e6ce97d7f04c8"
 
   url "https://github.com/patschuh/KafkaEsque/releases/download/v#{version}/kafkaesque-#{version}.dmg"
   name "KafkaEsque"
